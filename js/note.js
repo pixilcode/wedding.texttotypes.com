1568a1709a9af54429ea8d4847ddec6dfd07ff8a
Brendon Bown <brendon.bown@gmail.com>
2024-07-15 20:52:44 -0600

    temp

---
+++ js/note.js
@@ -0,0 +1,27 @@
+const noteFormApi = '/api/note/form'
+
+const unloadedForm = document.querySelector('form.unloaded')
+const loadingMessage = document.querySelector('.loading-message')
+
+function setLoadingMessage(message) {
+	const messageP = loadingMessage.querySelector('p')
+	messageP.textContent = message
+}
+
+function hideLoadingMessage() {
+	loadingMessage.display = 'none'
+}
+
+function showLoadingMessage() {
+	loadingMessage.display = 'flex'
+}
+
+// load the form
+fetch(noteFormApi)
+	.then(response => response.text())
+	.then(html => {
+		unloadedForm.outerHTML = html
+	})
+	.catch(error => {
+		console.error(error)
+	})
